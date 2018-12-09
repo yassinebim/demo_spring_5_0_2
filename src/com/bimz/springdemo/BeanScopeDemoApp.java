ffb12a21a55d72577756effb15a2dc712f460c78
yassinebim <yassine.bimezzagh@gmail.com>
2018-12-09 12:58:09 +0100

    Bean scopes experiments prototype Scope

---
+++ src/com/bimz/springdemo/BeanScopeDemoApp.java
@@ -0,0 +1,26 @@
+package com.bimz.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanScopeDemoApp {
+
+	public static void main(String[] args) {
+		// load the spring configuration file
+				ClassPathXmlApplicationContext context = 
+						new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
+						
+				// retrieve bean from spring container
+				Coach theCoach = context.getBean("myCoach", Coach.class);
+				
+				
+				Coach theSecondCoach = context.getBean("myCoach", Coach.class);
+
+				
+				// call methods on the bean
+				System.out.println("Is theCoach And theSecondCoach Pointing same reference : "+ (theCoach==theSecondCoach));
+				
+				// close the context
+				context.close();
+	}
+
+}
